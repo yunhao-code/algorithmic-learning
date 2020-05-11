b12b9887e1adfce63f9397b1914ffb670583750a
wei <weiyh@iquanwai.com>
2020-05-11 16:33:56 +0800

    广度优先算法、深度优先算法

---
+++ base-learning/src/main/java/yunhao/pojo/TreeNode.java
@@ -0,0 +1,30 @@
+package yunhao.pojo;
+
+public class TreeNode {
+    public int data;
+    public TreeNode leftNode;
+    public TreeNode rightNode;
+
+    public TreeNode() {
+
+    }
+
+    public TreeNode(int d) {
+        data=d;
+    }
+
+    public TreeNode(TreeNode left,TreeNode right,int d) {
+        leftNode = left;
+        rightNode = right;
+        data = d;
+    }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "data=" + data +
+                ", leftNode=" + leftNode +
+                ", rightNode=" + rightNode +
+                '}';
+    }
+}
